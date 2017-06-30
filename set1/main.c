c9b945ce960321c0ac55f5be772e34b3b7b900f2
Evan Duffy <eduffy@vmware.com>
2017-06-30 14:30:31 -0400

    Initial Commit. Added section 1

---
+++ set1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc,char ** argv){
+	
+}
+
+
+char* hexStringToCharArray(char *string){
+	char *ret = (char *)calloc(strlen(string)/2 + 1,sizeof(char));
+	for (int i = 0; i < (strlen(string)/2)*2; i += 2){
+			
+	}
+	return 0;
+}
+
+
