990a412510622b0f2884ac4fdc77e659b95240ef
Evan Duffy <eduffy@vmware.com>
2017-07-03 11:44:17 -0400

    Fixed conversion algorithm, also relized i forgot to encode 0 to 9, fixed that.

--- set1/hexto64.c
+++ set1/hexto64.c
@@ -7,10 +7,10 @@ char* hexToBase64String(char* hex, int len){
 	int i;
 	int h = 0;
 	for (i = 0; i < len; i += 3){
-		ret[h++] = numberToBase64Char(hex[i] & 0x3F);
-		ret[h++] = numberToBase64Char(((hex[i] & ~0x3F) >> 6) + ((hex[i+1] & 0xF) << 2));
-		ret[h++] = numberToBase64Char(((hex[i+1] & 0xF0) >> 4) + ((hex[i+2] & 0x3) << 2));
-		ret[h++] = numberToBase64Char((hex[i+2] & ~0x03) >> 2);
+		ret[h++] = numberToBase64Char((hex[i] & ~0x03) >> 2);
+		ret[h++] = numberToBase64Char(((hex[i+1] & 0xF0) >> 4) + ((hex[i] & 0x3) << 4));
+		ret[h++] = numberToBase64Char(((hex[i+2] & 0xC0) >> 6) + ((hex[i+1] & 0xF) << 2));
+		ret[h++] = numberToBase64Char(hex[i+2] & 0x3F);
 	}
 	ret[i+1] = 0;
 	return ret;
@@ -20,8 +20,11 @@ char numberToBase64Char(char number){
 	if (number >=0 && number <= 25){
 		return number + 'A';
 	}
-	if (number >=26 && number <= 61){
-		return number + 'a' - 26;
+	if (number >=26 && number <= 51){
+		return (number-26) + 'a';
+	}
+	if (number >= 52 && number <= 61){
+		return (number-52) + '0';
 	}
 	else if (number == 62) return '+';
 	else return '/';
--- set1/main.c
+++ set1/main.c
@@ -5,8 +5,8 @@
 
 
 int main(int argc,char ** argv){
-	//char *hex = hexStringToCharArray("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
-	char *hex = "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.";
+	char *hex = hexStringToCharArray("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+	//char *hex = "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.";
 	int len = strlen(hex);
 	printf("%s\n", hexToBase64String(hex, len));
 	return 0;
